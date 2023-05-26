ab64fca25270e241a584a8d56928e8e3555ff160
keixjk <kayegallogo@gmail.com>
2023-05-26 20:32:29 +0800

    asked user for num1 and num2

--- samplecalculator.py
+++ samplecalculator.py
@@ -16,4 +16,7 @@ def divide (num1, num2):
 def calculator():
         # ask user for operation
         operation = input("choose an operation (+, -, *, /): ")
-        
+        # ask user for num1
+        num1 = float(input("enter the first number"))
+        # ask user for num2
+        num2 = float(input("enter the second number"))
